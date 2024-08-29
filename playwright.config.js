72a458d63993f1ec623cbb0eccdcd0b50eb5dd03
swatiradia <swatimradia@gmail.com>
2024-08-29 14:56:52 -0400

    Basic tests and configurations

--- playwright.config.js
+++ playwright.config.js
@@ -17,6 +17,8 @@ module.exports = defineConfig({
   expect:{
     timeout: 5000
   },
+
+
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -28,6 +30,8 @@ module.exports = defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
@@ -82,6 +86,8 @@ module.exports = defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+
+  
 });
 
 
